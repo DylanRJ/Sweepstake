89db6ce14b53d2528f383a2c1a470aa1463f3cae
Dylan James <90717175+DylanRJ@users.noreply.github.com>
2022-01-18 12:19:20 +0000

    Revert "Bootstrap"

--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Players", type: :request do
   describe "GET /index" do
-
+    it "returns http success" do
+      get "/players/index"
+      expect(response).to have_http_status(:success)
+    end
   end
 
 end
